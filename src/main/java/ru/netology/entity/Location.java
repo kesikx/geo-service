fd558567c8f226651090172dae8c67df686358fd
Rashid Khamitov <rashid.khamitov@netcracker.com>
2022-06-20 18:22:14 +0400

    Add equals() and hashCode() to Location to support comparison in auto-tests

--- src/main/java/ru/netology/entity/Location.java
+++ src/main/java/ru/netology/entity/Location.java
@@ -29,6 +29,28 @@ public class Location {
         return street;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Location location = (Location) o;
+
+        if (builing != location.builing) return false;
+        if (city != null ? !city.equals(location.city) : location.city != null) return false;
+        if (country != location.country) return false;
+        return street != null ? street.equals(location.street) : location.street == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = city != null ? city.hashCode() : 0;
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (street != null ? street.hashCode() : 0);
+        result = 31 * result + builing;
+        return result;
+    }
+
     public int getBuiling() {
         return builing;
     }
